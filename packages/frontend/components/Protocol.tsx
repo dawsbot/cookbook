2a69ed5d830308044464117452f6d6821b921839
dawsbot <3408480+dawsbot@users.noreply.github.com>
2023-09-24 00:09:17 -0400

    categories

--- packages/frontend/components/Protocol.tsx
+++ packages/frontend/components/Protocol.tsx
@@ -7,12 +7,14 @@ type Step = {
 };
 type Protocol = {
   title: string;
+  categories: ReadonlyArray<string>;
   description: string;
   steps: ReadonlyArray<Step>;
 };
 
 const steps: Protocol = {
-  title: "Protect your funds with a Multisig",
+  title: "Create a personal Multisig",
+  categories: ["security", "solo"],
   description:
     "In your DeFi journey, you'll need to keep your funds safe. One of the best ways is using a multisig so that if any one wallet is compromised, your funds are safe!",
   steps: [
@@ -31,12 +33,36 @@ const steps: Protocol = {
     },
   ],
 };
+type PillProps = {
+  category: string;
+  index: number;
+};
+const colorOptions = ["#A3FFBC", "#C2CCFF", "#FFC2F2"];
+const Pill: React.FunctionComponent<PillProps> = ({ category, index }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: colorOptions[index],
+        borderRadius: "24px",
+        padding: "4px 16px",
+        margin: "4px",
+      }}
+    >
+      {category}
+    </div>
+  );
+};
 export const Protocol: React.FunctionComponent = () => {
   const [activeIndex] = useState(0);
   return (
     <>
       <h1>{steps.title}</h1>
       <p>{steps.description}</p>
+      <div style={{ display: "flex" }}>
+        {steps.categories.map((category, index) => (
+          <Pill category={category} index={index} />
+        ))}
+      </div>
       {steps.steps.map((step, index) => {
         return (
           <Card
